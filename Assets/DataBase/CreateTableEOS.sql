bf79464e694da8902543db7bad27416e2e18771f
buenex <gabrielbs98@hotnail.com>
2018-11-08 17:12:33 -0200

    DATA

---
+++ Assets/DataBase/CreateTableEOS.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE EOSDatabase
+
+CREATE TABLE Jogador(
+	id INT PRIMARY KEY IDENTITY(0,1),
+	nome VARCHAR(50) not null,
+	login VARCHAR(15)not null,
+	senha VARCHAR(30) not null,
+	email VARCHAR(80) not null
+)
+
+CREATE TABLE Location(
+	id INT PRIMARY KEY,
+	posX FLOAT,
+	poxy FLOAT,
+	posZ FLOAT,
+	rotX FLOAT,
+	rotY FLOAT,
+	rotZ FLOAT,
+	CONSTRAINT FK_Jogador FOREIGN KEY(id) REFERENCES Jogador(id)
+)
+
+CREATE TABLE Inventory(
+	id INT PRIMARY KEY,
+	slot1 INT,
+	slot2 INT,
+	slot3 INT,
+	slot4 INT,
+	slot5 INT,
+	slot6 INT,
+	flashLight FLOAT,
+	CONSTRAINT FK_Jogador FOREIGN KEY (id) REFERENCES JOGADOR(id)
+)
+
